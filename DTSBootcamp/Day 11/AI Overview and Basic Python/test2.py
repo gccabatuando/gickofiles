6b7d797c8dfe263bb811ff09408c841bc2575b66
Cabatuando <gicko.c.cabatuando@accenture.com>
2019-07-15 18:05:41 +0800

    ADD DTS BOOTCAMP

---
+++ DTSBootcamp/Day 11/AI Overview and Basic Python/test2.py
@@ -0,0 +1,13 @@
+a = 20
+b = 20
+
+if ( a is b ):
+   print("Line 1 - a and b have same identity")
+else:
+   print("Line 1 - a and b do not have same identity")
+
+if ( id(a) == id(b) ):
+   print("Line 2 - a and b have same identity")
+else:
+   print("Line 2 - a and b do not have same identity")
+
